64cbd63aa78f5e57823ee7e0fd38a4c0734beba9
Rodrigo Castro <rodrigondec@gmail.com>
2016-10-21 17:52:03 -0300

    criado listarProcessos()

--- src/main/java/dao/GraduadoDAO.java
+++ src/main/java/dao/GraduadoDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -7,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import model.Graduado;
+import model.ProcessoSeletivo;
 
 
 @Stateless
@@ -27,13 +30,27 @@ public class GraduadoDAO {
 		em.remove(x);
 	}
 	
-	public Graduado getGraduado(int idusuario){
+	public Graduado buscarGraduado(int idusuario){
 		String qs= "select g from Graduado g where g.usuario.idusuario= :idusuario";
 		Query q = em.createQuery(qs);
 		q.setParameter("idusuario", idusuario);
 		
 		try{
 			return (Graduado) q.getSingleResult();
+		} catch(NoResultException e) {
+			return null;
+		}
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	public List<ProcessoSeletivo> listarProcessos(int idgraduado){
+		String qs = "select curso, data_inicio, data_fim, edital from processo_seletivo natural join vaga natural join inscricao natural join graduado where idgraduado = :idgraduado";
+		Query q = em.createNativeQuery(qs);
+		q.setParameter("idgraduado", idgraduado);
+		
+		try{
+			return (List<ProcessoSeletivo>) q.getResultList();
 		} catch(NoResultException e) {
 			return null;
 		}
