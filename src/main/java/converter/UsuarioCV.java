cf44ed47db561b440b60260b5b0f519831e18c61
Rodrigo Castro <rodrigondec@gmail.com>
2016-10-23 18:12:00 -0300

    adicionado FacesConverter

---
+++ src/main/java/converter/UsuarioCV.java
@@ -0,0 +1,38 @@
+package converter;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+import dao.UsuarioDAO;
+import model.Usuario;
+
+@ManagedBean
+@RequestScoped
+@FacesConverter("UsuarioCV")
+public class UsuarioCV implements Converter{
+	@Inject  
+    private UsuarioDAO usuarioDAO;
+	
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String idusuario) {
+		int id = Integer.parseInt(idusuario);
+		
+		Usuario usuario = usuarioDAO.buscarIdusuario(id);
+		
+		return usuario;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object usuario) {
+		if (!(usuario instanceof Usuario)) {
+            return null;
+        }
+        return String.valueOf(((Usuario) usuario).getIdusuario());
+	}
+
+}
