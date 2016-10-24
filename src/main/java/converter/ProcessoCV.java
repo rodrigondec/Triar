4391531a4c6fc16291f1ac692ad953cc399d0878
Rodrigo Castro <rodrigondec@gmail.com>
2016-10-23 21:55:35 -0300

    adicionado converter para processo seletivo

---
+++ src/main/java/converter/ProcessoCV.java
@@ -0,0 +1,38 @@
+package converter;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+import dao.ProcessoSeletivoDAO;
+import model.ProcessoSeletivo;
+
+@ManagedBean
+@RequestScoped
+@FacesConverter("UsuarioCV")
+public class ProcessoCV implements Converter{
+	@Inject
+	ProcessoSeletivoDAO processoDAO;
+
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String idprocesso) {
+		int id = Integer.parseInt(idprocesso);
+		
+		ProcessoSeletivo processo = processoDAO.buscarIdprocesso(id);
+		System.out.println(processo.getCurso());
+		return processo;
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object processo) {
+		if (!(processo instanceof ProcessoSeletivo)) {
+            return null;
+        }
+        return String.valueOf(((ProcessoSeletivo) processo).getIdprocesso());
+	}
+
+}
