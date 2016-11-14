6a767a41657575918a7b08d929bbdf764a579723
Rodrigo Castro <rodrigondec@gmail.com>
2016-11-14 15:23:12 -0300

    processoService cadastra vaga

--- src/main/java/service/ProcessoSeletivoService.java
+++ src/main/java/service/ProcessoSeletivoService.java
@@ -6,15 +6,24 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import dao.ProcessoSeletivoDAO;
+import dao.VagaDAO;
 import model.ProcessoSeletivo;
+import model.Vaga;
 
 @Stateless
 public class ProcessoSeletivoService {
 	@Inject
 	private ProcessoSeletivoDAO processoDAO;
+	
+	@Inject 
+	private VagaDAO vagaDAO;
 
 	public void cadastrarProcessoSeletivo(ProcessoSeletivo processo) {
 		processoDAO.salvar(processo);
+	}
+	
+	public void cadastrarVaga(Vaga vaga){
+		vagaDAO.salvar(vaga);
 	}
 
 	public List<ProcessoSeletivo> listarProcessos() {
