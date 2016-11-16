c43f84cafcb65dfbb24b2dd17e91b59d48cff1d5
Rodrigo Castro <rodrigondec@gmail.com>
2016-11-16 00:17:36 -0300

    adicionado MB para cadastrar inscricao

---
+++ src/main/java/controller/CadastrarInscricaoMB.java
@@ -0,0 +1,86 @@
+package controller;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import model.Inscricao;
+import model.ProcessoSeletivo;
+import model.Vaga;
+import service.InscricaoService;
+import session.SessionContext;
+
+@ManagedBean
+@RequestScoped
+public class CadastrarInscricaoMB {
+	private Inscricao inscricao;
+
+	@EJB
+	InscricaoService inscricaoService;
+	
+	private ProcessoSeletivo processo;
+	private Vaga vaga;
+	private List<Vaga> vagas;
+
+	public CadastrarInscricaoMB(){
+		setInscricao(new Inscricao());
+	}
+
+	public Inscricao getInscricao() {
+		return inscricao;
+	}
+
+	public void setInscricao(Inscricao inscricao) {
+		this.inscricao = inscricao;
+	}
+	
+	public List<Vaga> getVagas() {
+		return vagas;
+	}
+
+	public void setVagas(List<Vaga> vagas) {
+		this.vagas = vagas;
+	}
+
+	public ProcessoSeletivo getProcesso() {
+		return processo;
+	}
+
+	public void setProcesso(ProcessoSeletivo processo) {
+		this.processo = processo;
+	}
+	
+	public String geturlProcesso(){
+		return "/interna/graduado/registrar_inscricao.jsf";
+	}
+	
+	public String geturlVaga(){
+		return "/interna/graduado/registrar_vaga.jsf";
+	}
+	
+	public String armazenar(){
+		vagas = inscricaoService.listarVagas(processo.getIdprocesso());
+		return geturlVaga();
+	}
+	
+	public String cadastrar(){
+		//TODO PEGAR VAGA
+		inscricaoService.cadastrarInscricao(inscricao);
+		
+		inscricao = new Inscricao();
+		
+		SessionContext.getInstance().removeAttribute("processo");
+		
+		return "/interna/graduado/index.jsf";
+	}
+
+	public Vaga getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(Vaga vaga) {
+		this.vaga = vaga;
+	}
+}
